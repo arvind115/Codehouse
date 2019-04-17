95fa2f951bfd5a7134636cc6b8f2a0bec036c66c
Arvind Kumar Singh <samratarvind114@gmail.com>
2019-04-17 10:58:10 +0530

    Update xor_&_or.cpp
    added query() function

--- SEGTREE/xor_&_or.cpp
+++ SEGTREE/xor_&_or.cpp
@@ -1,24 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int merge(int a,int b,int l){
-    if( l % 2 == 0)//xor 
-        int ans = a^b;
-    if( l % 2 == 1)//or
-        int ans = a|b;        
-    return ans;
-}
-void build(int seg[],int a[],int low,int high, int pos,int l){
+void build(int seg[],int a[],int low,int high, int pos){
     if(low==high){
         seg[pos]=a[low];
         return;
     }
     int mid=(low+high)/2;
-    build(seg,a,low,mid,2*pos+1,l+1);
-    build(seg,a,mid+1,2*pos+2,l+1);
-    seg[pos]=merge(seg[2*pos+1],seg[2*pos+2],l);
+    build(seg,a,low,mid,2*pos+1);
+    build(seg,a,mid+1,high,2*pos+2);
+    seg[pos]=min(seg[2*pos+1],seg[2*pos+2]);
 }
-int query(int seg[],int qlow, int qhigh, int low, int high, int pos)
+int query(int seg[],int qlow, int qhigh, int low, int high, int pos){
+    if( qlow>=low && high <=qhigh) ///totally within the asked range
+        return seg[pos];
+    if( low > high)
+        return INT_MAX;
+    if( low > qhigh || high < qlow )
+        return INT_MAX;
+    int mid = (low+high)/2;
+    return min(query(seg,qlow,qhigh,low,mid,2*pos+1),query(seg,qlow,qhigh,mid+1,high,2*pos+2));
+}
 int main() {
-	cout<<"GfG!";
+	int n;///no of elements in seg tree
+	cin>>n;
+	int a[n+2];
+	for(int i=0; i<n; i++)
+	    cin>>a[i];
+	int nextPowOf2 = (int)round(pow(2, ceil(log2(n))));
+    int size = 2 * nextPowOf2 - 1;
+	int seg[size];
+	build(seg,a,0,n-1,0);
+	cout<<query(seg,0,n-1,0,n-1,0);
 	return 0;
 }
