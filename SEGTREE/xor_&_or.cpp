b17f841a0ef98269248474c8825105ce9f4cd6be
Arvind Kumar Singh <samratarvind114@gmail.com>
2019-04-19 19:54:23 +0530

    finally got everything right. No need to implement lazy updation, because the problems requires point updates only

--- SEGTREE/xor_&_or.cpp
+++ SEGTREE/xor_&_or.cpp
@@ -1,27 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int merge(int a,int b,int l){
-    cout<<"in merge() with "<<a<<","<<b<<endl;
-    cout<<"l="<<l<<endl;
-
-    int ans;
-    if( l % 2 == 0 )/// XOR
-     ans = a^b;
-    else ans = a|b; ///OR
-    return ans;
+int merge(int a,int b,int l,int N){
+    if( ( N%2==0 && l%2==0) || ( N%2!=0 && l%2!=0 ) )
+        return a|b;
+    else if( ( N%2!=0 && l%2==0) || ( N%2==0 && l%2!=0) )
+        return a^b;
 }
-void build(int seg[],int a[],int low,int high, int pos,int l){
-    cout<<"in build() with low="<<low<<", high="<<high<<" ,l="<<l<<endl;
+void build(int seg[],int a[],int low,int high, int pos,int l,int N){
     if(low==high){
         seg[pos]=a[low];
         return;
     }
     int mid=(low+high)/2;
-    build(seg,a,low,mid,2*pos+1,l+1);
-    build(seg,a,mid+1,high,2*pos+2,l+1);
-    seg[pos]=merge(seg[2*pos+1],seg[2*pos+2],l);
+    build(seg,a,low,mid,2*pos+1,l+1,N);
+    build(seg,a,mid+1,high,2*pos+2,l+1,N);
+    seg[pos]=merge(seg[2*pos+1],seg[2*pos+2],l,N);
 }
-int query(int seg[],int qlow, int qhigh, int low, int high, int pos,int l){
+int query(int seg[],int qlow, int qhigh, int low, int high, int pos,int l,int N){
     if( qlow>=low && high <=qhigh) ///totally within the asked range
         return seg[pos];
     /// a|0 = a, a^0 = a
@@ -30,9 +25,9 @@ int query(int seg[],int qlow, int qhigh, int low, int high, int pos,int l){
     if( low > qhigh || high < qlow )
         return 0;
     int mid = (low+high)/2;
-    return merge(query(seg,qlow,qhigh,low,mid,2*pos+1,l+1),query(seg,qlow,qhigh,mid+1,high,2*pos+2,l+1),l);
+    return merge(query(seg,qlow,qhigh,low,mid,2*pos+1,l+1,N),query(seg,qlow,qhigh,mid+1,high,2*pos+2,l+1,N),l,N);
 }
-void update(int seg[],int qlow, int qhigh,int low, int high,int val, int pos,int l){///point update
+void update(int seg[],int qlow, int qhigh,int low, int high,int val, int pos,int l,int N){///point update
     if( qlow == low && qhigh == high ){
         seg[pos]=val;
         return;
@@ -42,13 +37,14 @@ void update(int seg[],int qlow, int qhigh,int low, int high,int val, int pos,int
     if( low > qhigh || high < qlow )
         return;
     int mid = (low+high)/2;
-    update(seg,qlow,qhigh,low,mid,val,2*pos+1,l+1);
-    update(seg,qlow,qhigh,mid+1,high,val,2*pos+2,l+1);
-    seg[pos]=merge(seg[2*pos+1],seg[2*pos+2],l);
+    update(seg,qlow,qhigh,low,mid,val,2*pos+1,l+1,N);
+    update(seg,qlow,qhigh,mid+1,high,val,2*pos+2,l+1,N);
+    seg[pos]=merge(seg[2*pos+1],seg[2*pos+2],l,N);
 }
 int main() {
 	int n,m;
 	cin>>n>>m;
+	int N=n;
 	n=int(pow(2,n));
 	int a[n+2];
 	for(int i=0; i<n; i++)
@@ -56,15 +52,13 @@ int main() {
 	int nextPowOf2 = (int)round(pow(2, ceil(log2(n))));
     int size = 2 * nextPowOf2 - 1;
 	int seg[size];
-	build(seg,a,0,n-1,0,1);
+	build(seg,a,0,n-1,0,1,N);
 	for(int i=0; i<m; i++){
         int p,val;
         cin>>p>>val;
-        update(seg,p-1,p-1,0,n-1,val,0,1); ///point update
-        cout<<query(seg,0,n-1,0,n-1,0,0)<<endl;
+        update(seg,p-1,p-1,0,n-1,val,0,1,N); ///point update
+        cout<<query(seg,0,n-1,0,n-1,0,1,N)<<endl;
 	}
-//	update(seg,0,0,0,n-1,4,0);
-//	cout<<query(seg,0,n-1,0,n-1,0)<<endl;
 	return 0;
 }
 /*
